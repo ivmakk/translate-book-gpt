0cee172ea85bd480f76d2a0a2c470597d8ac6976
ivmakk <ivmakkomg@gmail.com>
2025-01-18 12:00:15 +0200

    Added new chunk strategy (split by new lines); Added HTML encoding to fix prompting without success results; Updated dependencies and added unit tests for HTML splitting and locale functions;

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import html
+import langcodes
+import tiktoken
 import argparse, \
        re, \
        yaml
@@ -7,7 +10,24 @@ from ebooklib import epub
 from bs4 import BeautifulSoup
 from openai import OpenAI
 
+## VS Code Debugging
+# import ptvsd
+#
+# # Allow other computers to attach to ptvsd at this IP address and port.
+# #
+# # netstat -ano | findstr :5678
+# # taskkill /PID <PID> /F
+# #
+# # OR
+# #
+# # Stop-Process -Id (Get-NetTCPConnection -LocalPort 5678).OwningProcess -Force
+# ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
 
+# # Pause the program until a remote debugger is attached
+# print("Waiting for debugger to attach...")
+# ptvsd.wait_for_attach()
+
+GPT_MODEL_NAME = 'gpt-4o-mini'
 
 def read_config(config_file):
     with open(config_file, 'r') as f:
@@ -42,6 +62,29 @@ def split_html_by_sentence(html_str, max_chunk_size=10000):
 
     return chunks
 
+def split_html_by_newline(html_str, max_chunk_size=10000):
+    chunks = []
+    lines = html_str.split('\n')
+
+    if len(html_str) == 0:
+        return chunks
+
+    current_chunk = ""
+
+    for line in lines:
+        if len(current_chunk) + len(line) > max_chunk_size:
+            if current_chunk:
+                chunks.append(current_chunk)
+            current_chunk = line
+        else:
+            if current_chunk:
+                current_chunk += '\n'
+            current_chunk += line
+    
+    if current_chunk:
+        chunks.append(current_chunk)
+
+    return chunks
 
 def system_prompt(from_lang, to_lang):
     p  = "You are an %s-to-%s translator. " % (from_lang, to_lang)
@@ -51,21 +94,47 @@ def system_prompt(from_lang, to_lang):
 
 def translate_chunk(client, text, from_lang='EN', to_lang='PL'):
     response = client.chat.completions.create(
-        model='gpt-4o',
+        model=GPT_MODEL_NAME,
         temperature=0.2,
         messages=[
-            { 'role': 'system', 'content': system_prompt(from_lang, to_lang) },
-            { 'role': 'user', 'content': text },
+            { 'role': 'developer', 'content': system_prompt(from_lang, to_lang) },
+            { 'role': 'user', 'content':  html.escape(text) },
         ]
     )
 
     translated_text = response.choices[0].message.content
-    return translated_text
+    decoded_text = html.unescape(translated_text)
 
+    original_lines = text.count('\n')
+    decoded_lines = decoded_text.count('\n')
+
+    if abs(original_lines - decoded_lines) > 1:
+        print("Warning: The number of lines in the original text (%d) and the decoded text (%d) are different." % (original_lines, decoded_lines))
+
+    return decoded_text
+
+
+def translate_toc(client, toc, from_lang='EN', to_lang='PL'):
+    toc_list = list(toc)
+    toc_text = "\n".join([item.title.strip() for item in toc_list if isinstance(item, epub.Link)])
+    
+    translated_toc_text = translate_text(client, toc_text, from_lang, to_lang)
+    translated_titles = [title.strip() for title in translated_toc_text.split('\n')]
+    
+    translated_toc = []
+    title_index = 0
+    for item in toc_list:
+        if isinstance(item, epub.Link):
+            translated_toc.append(epub.Link(item.href, translated_titles[title_index], item.uid))
+            title_index += 1
+        else:
+            translated_toc.append(item)
+    
+    return tuple(translated_toc)
 
 def translate_text(client, text, from_lang='EN', to_lang='PL'):
     translated_chunks = []
-    chunks = split_html_by_sentence(text)
+    chunks = split_html_by_newline(text)
 
     for i, chunk in enumerate(chunks):
         print("\tTranslating chunk %d/%d..." % (i+1, len(chunks)))
@@ -77,8 +146,12 @@ def translate_text(client, text, from_lang='EN', to_lang='PL'):
 def translate(client, input_epub_path, output_epub_path, from_chapter=0, to_chapter=9999, from_lang='EN', to_lang='PL'):
     book = epub.read_epub(input_epub_path)
 
+    book.set_unique_metadata('DC', 'language', langcodes.standardize_tag(to_lang))
+
     current_chapter = 1
     chapters_count = len([i for i in book.get_items() if i.get_type() == ebooklib.ITEM_DOCUMENT])
+
+    book.toc = translate_toc(client, book.toc, from_lang, to_lang)
 
     for item in book.get_items():
         if item.get_type() == ebooklib.ITEM_DOCUMENT:
@@ -86,12 +159,62 @@ def translate(client, input_epub_path, output_epub_path, from_chapter=0, to_chap
                 print("Processing chapter %d/%d..." % (current_chapter, chapters_count))
                 soup = BeautifulSoup(item.content, 'html.parser')
                 translated_text = translate_text(client, str(soup), from_lang, to_lang)
-                item.content = translated_text.encode('utf-8')
+
+                soup.body.clear()
+                translated_soup = BeautifulSoup(translated_text, 'html.parser')
+                soup.body.extend(translated_soup.body.contents)
+
+                if soup.title and translated_soup.title:
+                    soup.title.string = translated_soup.title.string
+
+                item.content = str(soup).encode('utf-8')
 
             current_chapter += 1
 
     epub.write_epub(output_epub_path, book, {})
 
+
+def show_chunks(input_epub_path):
+    book = epub.read_epub(input_epub_path)
+
+    encoding = tiktoken.encoding_for_model(GPT_MODEL_NAME)
+
+    book_total_tokens = 0
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            content_str = str(BeautifulSoup(item.content, 'html.parser'))
+            chunks = split_html_by_newline(content_str)
+
+            print("Document: %s" % item.get_name())
+            
+            document_total_tokens = 0
+            for i, chunk in enumerate(chunks):
+                tokens = encoding.encode(chunk)
+                print("Chunk %d/%d (Tokens: %d):" % (i+1, len(chunks), len(tokens)))
+
+                document_total_tokens += len(tokens)
+                
+                lines = chunk.split('\n')
+                if len(lines) > 6:
+                    print('\n'.join(lines[:3]) + "\n...\n" + '\n'.join(lines[-3:]) + "\n\n")
+                else:
+                    print('\n'.join(lines) + "\n\n")
+
+                book_total_tokens += document_total_tokens
+
+            print("Total tokens in document: %d\n" % document_total_tokens)
+
+            input_price = calculate_input_price(document_total_tokens, GPT_MODEL_NAME)
+            output_price = calculate_output_price(document_total_tokens, GPT_MODEL_NAME)
+            total_price = input_price + output_price
+            print("Price for input: $%.2f, Price for output: $%.2f, Total price: $%.2f" % (input_price, output_price, total_price))
+            
+            print("--------------------------------------------------\n")
+
+    input_price = calculate_input_price(book_total_tokens, GPT_MODEL_NAME)
+    output_price = calculate_output_price(book_total_tokens, GPT_MODEL_NAME)
+    total_price = input_price + output_price
+    print("Total book price for input: $%.2f, Price for output: $%.2f, Total price: $%.2f" % (input_price, output_price, total_price))
 
 def show_chapters(input_epub_path):
     book = epub.read_epub(input_epub_path)
@@ -109,6 +232,35 @@ def show_chapters(input_epub_path):
 
             current_chapter += 1
 
+def calculate_input_price(tokens, model_name):
+    """See https://openai.com/api/pricing/ for the latest pricing information."""
+    model_prices_per_million = {
+        'gpt-4o': 2.50,
+        'gpt-4o-mini': 0.150
+    }
+
+    if model_name not in model_prices_per_million:
+        return 0
+
+    price_per_million = model_prices_per_million[model_name]
+    price_per_token = price_per_million / 1_000_000
+
+    return tokens * price_per_token
+
+def calculate_output_price(tokens, model_name):
+    """See https://openai.com/api/pricing/ for the latest pricing information."""
+    model_prices_per_million = {
+        'gpt-4o': 10.00,
+        'gpt-4o-mini': 0.6
+    }
+
+    if model_name not in model_prices_per_million:
+        return 0
+
+    price_per_million = model_prices_per_million[model_name]
+    price_per_token = price_per_million / 1_000_000
+
+    return tokens * price_per_token
 
 
 if __name__ == "__main__":
@@ -130,14 +282,18 @@ if __name__ == "__main__":
     parser_show = subparsers.add_parser('show-chapters', help='Show the list of chapters.')
     parser_show.add_argument('--input', required=True, help='Input file path.')
 
+    # Create the parser for the "show-chunks" mode
+    parser_show_chunks = subparsers.add_parser('show-chunks', help='Show the chunks of the book.')
+    parser_show_chunks.add_argument('--input', required=True, help='Input file path.')
+
     # Parse the arguments
     args = parser.parse_args()
 
     # Call the appropriate function based on the mode
     if args.mode == 'translate':
         config = read_config(args.config)
-        from_chapter = int(args.from_chapter)
-        to_chapter = int(args.to_chapter)
+        from_chapter = int(args.from_chapter or 0)
+        to_chapter = int(args.to_chapter or 9999)
         from_lang = args.from_lang
         to_lang = args.to_lang
         openai_client = OpenAI(api_key=config['openai']['api_key'])
@@ -146,6 +302,9 @@ if __name__ == "__main__":
 
     elif args.mode == 'show-chapters':
         show_chapters(args.input)
+
+    elif args.mode == 'show-chunks':
+        show_chunks(args.input)
 
     else:
         parser.print_help()
---
+++ test_main.py
@@ -0,0 +1,62 @@
+﻿import unittest
+
+from main import split_html_by_newline
+from main import to_locale
+
+
+class TestSplitHtmlByNewline(unittest.TestCase):
+    def test_split_html_by_newline_basic(self):
+        html_str = "This is a line.\nThis is another line."
+        expected = ["This is a line.\nThis is another line."]
+        result = split_html_by_newline(html_str)
+        self.assertEqual(result, expected)
+
+    def test_split_html_by_newline_with_max_chunk_size(self):
+        html_str = "This is a line.\nThis is another line.\nThis is yet another line."
+        expected = ["This is a line.\nThis is another line.", "This is yet another line."]
+        result = split_html_by_newline(html_str, max_chunk_size=50)
+        self.assertEqual(result, expected)
+
+    def test_split_html_by_newline_large_chunk(self):
+        html_str = ("This is a very long line that should be split into multiple chunks.\n" * 10)
+        result = split_html_by_newline(html_str, max_chunk_size=100)
+        self.assertTrue(len(result) > 1)
+
+    def test_split_html_by_newline_empty_string(self):
+        html_str = ""
+        expected = []
+        result = split_html_by_newline(html_str)
+        self.assertEqual(result, expected)
+
+    def test_split_html_by_newline_no_newline(self):
+        html_str = "This is a line without a newline"
+        expected = ["This is a line without a newline"]
+        result = split_html_by_newline(html_str)
+        self.assertEqual(result, expected)
+
+    def test_split_html_by_newline_with_html_tags(self):
+        html_str = "<p>This is a line.</p>\n<p>This is another line.</p>"
+        expected = ["<p>This is a line.</p>", "<p>This is another line.</p>"]
+        result = split_html_by_newline(html_str, 10)
+        self.assertEqual(result, expected)
+
+class TestToLocale(unittest.TestCase):
+    def test_to_locale_default(self):
+        self.assertEqual(to_locale(), 'en')
+
+    def test_to_locale_specific_language(self):
+        self.assertEqual(to_locale('fr'), 'fr')
+
+    def test_to_locale_with_country_code(self):
+        self.assertEqual(to_locale('en-US'), 'en-US')
+
+    def test_to_locale_invalid_code(self):
+        with self.assertRaises(ValueError):
+            to_locale('invalid-code')
+
+    def test_to_locale_empty_string(self):
+        with self.assertRaises(ValueError):
+            to_locale('')
+
+if __name__ == '__main__':
+    unittest.main()
